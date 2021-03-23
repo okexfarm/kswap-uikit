200103cad8cc11fb67fc69630d6979f873830dc7
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-24 02:40:04 +0530

    Update Logo.tsx

--- src/widgets/Menu/Logo.tsx
+++ src/widgets/Menu/Logo.tsx
@@ -54,11 +54,11 @@ const Logo: React.FC<Props> = ({ isPushed, togglePush, isDark, href }) => {
         )}
       </MenuButton>
       {isAbsoluteUrl ? (
-        <StyledLink as="a" href={href} aria-label="Mango home page">
+        <StyledLink as="a" href={href} aria-label="Kswap home page">
           {innerLogo}
         </StyledLink>
       ) : (
-        <StyledLink to={href} aria-label="Mango home page">
+        <StyledLink to={href} aria-label="Kswap home page">
           {innerLogo}
         </StyledLink>
       )}
