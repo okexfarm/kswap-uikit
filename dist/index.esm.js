eee30e82d326c8396c124eaf3dd36aceba407c93
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-26 13:01:54 +0530

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2486,7 +2486,7 @@ var ConnectModal = function (_a) {
     var login = _a.login, _b = _a.onDismiss, onDismiss = _b === void 0 ? function () { return null; } : _b;
     return (React.createElement(Modal, { title: "Connect to a wallet", onDismiss: onDismiss },
         connectors.map(function (entry, index) { return (React.createElement(WalletCard, { key: entry.title, login: login, walletConfig: entry, onDismiss: onDismiss, mb: index < connectors.length - 1 ? "8px" : "0" })); }),
-        React.createElement(HelpLink, { href: "https://docs.kswap.net/guides/faq#how-do-i-set-up-my-wallet-on-binance-smart-chain", external: true },
+        React.createElement(HelpLink, { href: "https://docs.pancakeswap.finance/guides/faq#how-do-i-set-up-my-wallet-on-binance-smart-chain", external: true },
             React.createElement(Icon$L, { color: "primary", mr: "6px" }),
             "Learn how to connect")));
 };
